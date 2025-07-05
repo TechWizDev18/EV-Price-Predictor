409a1c6ac0363340345845bcba1f352135ae05a9
TechWizDev18 <168648554+TechWizDev18@users.noreply.github.com>
2025-07-05 23:22:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -147,4 +147,5 @@ def get_expected_features():
 
 # Run the Flask application in debug mode
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
